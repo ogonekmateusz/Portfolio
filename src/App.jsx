a26803de355016a2175956e30c5fc0a094ac8f9a
DESKTOP-VA58A1P\Mateusz <ogonekmateusz69@gmail.com>
2025-01-26 15:13:34 +0100

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-const App = () => {
-  return ( 
-    <h1>Hello world!</h1>
-   );
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        navbar
+        <div className="content">
+          <Routes>
+            <Route path="/" element={"home"} />
+            <Route path="/about" element={"about"} />
+            <Route path="/contact" element={"contact"} />
+            <Route path="/projects" element={"section"} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
- 
+
 export default App;
